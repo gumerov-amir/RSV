008e2393c9cd0341b5b76f7831454a01e78494d0
Gumerov Amir Eduardovich <=>
2022-08-02 17:39:52 +0300

    added rounding to client, fixed mempercent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Query
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import psutil
@@ -8,22 +8,22 @@ app = FastAPI(title="SysView", description="A web app for monetoring system stat
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+def index(request: Request, timeout: Query[int] = 10000):
+    return templates.TemplateResponse("index.html", {"request": request, "timeout": timeout})
 
 @app.get("/info/")
 def info():
     mem = psutil.virtual_memory()
     disk = psutil.disk_usage("/")
     return {
-        "cputemperature": round(psutil.sensors_temperatures()["cpu_thermal"][0].current, 2),
-        "memtotal": round(mem.total, 2),
-        "memused": round(mem.used, 2),
-        "memavailable": round(mem.available, 2),
-        "mem_percent": round(mem.percent, 2),
-        "disktotal": round(disk.total, 2),
-        "diskused": round(disk.used, 2),
-        "diskavailable": round(disk.free, 2),
-        "diskpercent": round(disk.percent, 2),
+        "cputemperature": psutil.sensors_temperatures()["cpu_thermal"][0].current,
+        "memtotal": mem.total,
+        "memused": mem.used,
+        "memavailable": mem.available,
+        "mempercent": mem.percent,
+        "disktotal": disk.total,
+        "diskused": disk.used,
+        "diskavailable": disk.free,
+        "diskpercent": disk.percent,
         "cpupercent": psutil.cpu_percent(),
     }
